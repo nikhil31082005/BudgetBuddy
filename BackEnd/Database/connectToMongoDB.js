adc2cadaa00bba28ee147121b957e008da19db6d
nikhil31082005 <nsikarwar496@gmail.com>
2025-01-02 08:29:17 +0530

    mongodb atlas use

--- BackEnd/Database/connectToMongoDB.js
+++ BackEnd/Database/connectToMongoDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
 	try {
-		const conn = await mongoose.connect(`mongodb://localhost:27017/Personal_Expance_Tracker`);
+		const conn = await mongoose.connect(process.env.MONGO_URL);
 		console.log(`MongoDB connected: ${conn.connection.host}`);
 	} catch (error) {
 		console.error(`Error connection to mongoDB: ${error.message}`);
